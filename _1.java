f05e9b5fae3d6b5361ed36fa82fe91c5ffc96fac
Antonio Fava <afavajrbr80@gmail.com>
2023-05-28 17:18:55 -0400

    classe mãe

---
+++ _1.java
@@ -0,0 +1,54 @@
+public class _1 {
+    
+    protected String nome;
+    protected int idade;
+    protected String dataNascimento;
+    protected String registroGeral;
+    protected String numeroCpf;
+    protected String nomeMae;
+    protected String nomePai;
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public String getRegistroGeral() {
+        return registroGeral;
+    }
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+    public String getNomeMae() {
+        return nomeMae;
+    }
+    public String getNomePai() {
+        return nomePai;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public void setRegistroGeral(String registroGeral) {
+        this.registroGeral = registroGeral;
+    }
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    
+}
