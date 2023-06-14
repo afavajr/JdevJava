e5ba81f183f53664e8414033c69c6ff6c2f24270
Antonio Fava <afavajrbr80@gmail.com>
2023-06-14 17:46:22 -0400

    8

--- classes/Secretario.java
+++ classes/Secretario.java
@@ -1,11 +1,15 @@
 package classes;
 
-public class Secretario extends Pessoa {
+import classes.interfaces.PermitirAcesso;
+
+public class Secretario extends Pessoa implements PermitirAcesso {
     
 
     private String registro;
     private String nivelCargo;
     private String experiencia;
+    private String login;
+    private String senha;
     
     public String getRegistro() {
         return registro;
@@ -35,5 +39,25 @@ public class Secretario extends Pessoa {
        return 2000.00;
     }
 
+    @Override
+    public boolean autenticar() {
+
+        return login.equals("admin") && senha.equals("admin");
+    }
+
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
     
 }
